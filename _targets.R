64fc8ba0b17318c7be1d06b79f6f0be6bd6b94c4
Andy McAliley <wmcaliley@usgs.gov>
2021-09-27 15:57:33 -0600

    Add interactive leaflet map of state timeseries

--- _targets.R
+++ _targets.R
@@ -6,7 +6,8 @@ suppressPackageStartupMessages(library(dplyr))
 
 options(tidyverse.quiet = TRUE)
 tar_option_set(packages = c("tidyverse", "dataRetrieval", "urbnmapr",
-                            "rnaturalearth", "cowplot", "lubridate"))
+                            "rnaturalearth", "cowplot", "lubridate",
+                            "leaflet", "leafpop", "htmlwidgets"))
 
 # Load functions needed by targets below
 source("1_fetch/src/find_oldest_sites.R")
@@ -16,10 +17,11 @@ source("2_process/src/summarize_targets.R")
 source("3_visualize/src/map_sites.R")
 source("3_visualize/src/plot_site_data.R")
 source("3_visualize/src/plot_data_coverage.R")
+source("3_visualize/src/map_timeseries.R")
 
 # Configuration
-states <- c('WI','MN','MI', 'IL', 'IN')
-# states <- c('WI','MN','MI', 'IL', 'IN', 'IA')
+# states <- c('WI','MN','MI', 'IL', 'IN')
+states <- c('WI','MN','MI', 'IL', 'IN', 'IA')
 parameter <- c('00060')
 
 # Pull site data
@@ -49,20 +51,22 @@ list(
   # Combine tallies
   tar_combine(obs_tallies, mapped_by_state_targets[[3]], command = combine_obs_tallies(!!!.x)),
   # Summarize targets
-  tar_combine(
-    summary_state_timeseries_csv,
-    mapped_by_state_targets[[4]],
-    command = summarize_targets('3_visualize/log/summary_state_timeseries.csv', !!!.x),
-    format="file"
-  ),
+  tar_combine(summary_state_timeseries_csv, 
+              mapped_by_state_targets[[4]], 
+              command = summarize_targets('3_visualize/log/summary_state_timeseries.csv', !!!.x), 
+              format="file"),
   # Plot data coverage
   tar_target(data_coverage_png, 
              plot_data_coverage(obs_tallies, "3_visualize/out/data_coverage.png", parameter),
              format = "file"),
   # Map oldest sites
-  tar_target(
-    site_map_png,
-    map_sites("3_visualize/out/site_map.png", oldest_active_sites),
-    format = "file"
-  )
+  tar_target(site_map_png, 
+             map_sites("3_visualize/out/site_map.png", oldest_active_sites), 
+             format = "file"),
+  # Interactive map of oldest sites, with timeseries plots
+  tar_target(timeseries_map_html,
+             map_timeseries(site_info = oldest_active_sites,
+                            plot_info_csv = summary_state_timeseries_csv,
+                            out_file = "3_visualize/out/timeseries_map.html"),
+             format = "file")
 )
